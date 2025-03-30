67fea5b71b2518639fb6d7d9f98427ce6988bd78
Rishikesh Kumar <64651343+rishikeshkumar08@users.noreply.github.com>
2025-03-30 18:13:49 +0530

    Delete JobPortal directory

--- JobPortal/src/main/java/com/rishikesh/JobPortal/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rishikesh.JobPortal.repository;
-
-import com.rishikesh.JobPortal.model.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PostRepository extends MongoRepository<Post, String> {
-
-}
